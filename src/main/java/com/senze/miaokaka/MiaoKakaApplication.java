745cc3be3dfce9fcee979dbe53b01c2087f7decc
冉二飞 <2228514018@qq.com>
2026-04-22 00:10:54 +0800

    Initial commit

---
+++ src/main/java/com/senze/miaokaka/MiaoKakaApplication.java
@@ -0,0 +1,13 @@
+package com.senze.miaokaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaoKakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiaoKakaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senze/miaokaka/MiaoKakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senze.miaokaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaoKakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
